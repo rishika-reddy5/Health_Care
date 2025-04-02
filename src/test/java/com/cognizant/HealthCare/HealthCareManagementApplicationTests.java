d05fdc5406d242468fecb36e96c2025d97002153
Basheer <2395708@cognizant.com>
2025-04-02 14:26:34 +0530

    Project skeleton

---
+++ src/test/java/com/cognizant/HealthCare/HealthCareManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.HealthCare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCareManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
